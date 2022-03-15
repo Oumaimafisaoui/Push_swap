74c891f0dd49158e4862d648bd6ecb87a61272ce
Raven <48260689+Oumaimafisaoui@users.noreply.github.com>
2022-03-15 21:33:19 +0100

    Add files via upload

---
+++ push/atoi_check.c
@@ -0,0 +1,35 @@
+#include "push_swap.h"
+
+int is_digit(char n)
+{
+    if(n >= '0' && n <= '9')
+        return (1);
+    return (0);
+}
+
+int atoi_check(const char *s)
+{
+    long long n;
+    int sign;
+    int result;
+
+    sign = 1;
+    result = 0;
+    n = 0;
+    if(*s == '-' || *s == '+')
+    {
+            if(*s == '-')
+                sign = -1;
+            s++;
+    }
+    if(!is_digit(*s))
+        error();
+    while(*s)
+    {
+        n = n * 10 + (*s - 48); 
+        s++;
+    }
+    if (*s != '\0' || (n > INT_MAX))
+        error();
+    return (n * sign);    
+}
